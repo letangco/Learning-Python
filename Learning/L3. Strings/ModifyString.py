cbaab931c7cd79d06cf429ffa90ff244b3b9e9b1
TangCo <17520296@gm.uit.edu.vn>
2021-01-23 16:34:19 +0700

    String

---
+++ Learning/L3. Strings/ModifyString.py
@@ -0,0 +1,17 @@
+my_character = 'Trường Đại học Công nghệ thông tin TP. Hồ Chí Minh'
+
+print(my_character.upper())
+
+print(my_character.lower())
+
+# Remove whiteSpace in the string
+MyString = "  hello, World          "
+print(MyString)
+print(MyString.strip())
+
+# Replace String
+
+print('Replace String: ', MyString.strip().replace('o', 'O'))
+
+# Split String
+print(MyString.split('o'))
--- Learning/L3. Strings/l3.1 SlicingStrings.py
+++ Learning/L3. Strings/l3.1 SlicingStrings.py
@@ -1,4 +1,6 @@
 my_character = 'Trường Đại học Công nghệ thông tin TP. Hồ Chí Minh'
 
 # Slicing
-print(my_character[1:10])
+print(my_character[-6:-2])
+
+print(my_character[1:])
