06c69d73bb0246fe2c49631cc2b22673d90a24f9
TangCo <17520296@gm.uit.edu.vn>
2021-01-23 16:38:51 +0700

    String

---
+++ Learning/Boolean/index.py
@@ -0,0 +1,16 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
